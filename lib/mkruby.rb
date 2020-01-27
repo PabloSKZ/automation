3800b0c598bd48ffc47c6d12334e55a3e4f8e011
PabloSKZ <pablo.eskenazi@gmail.com>
2020-01-27 11:17:00 +0100

    + modif Luca Montaigut

--- lib/mkruby.rb
+++ lib/mkruby.rb
@@ -1,32 +1,42 @@
+#PRISE EN COMMANDE DU NOM DU DOSSIER DE PROJET
 abort("mkruby : no space in the folder name, you silly punk !") if ARGV.count > 1
 abort("mkruby : a name, god sake, GIVE ME A NAME !") if ARGV.empty?
-
+​
 input = ARGV.join
-
+​
+#CREATION DU DOSSIER
 Dir.mkdir(input)
-
-file = File.open(input + "/Gemfile", "w")
-
+​
+#CREATION DU DOSSIER LIB
+Dir.mkdir(input + "/lib")
+​
+#CREATION DU GEMFILE
+file_gemfile = File.open(input + "/Gemfile", "w")
+​
 gem = ["source \"https://rubygems.org\"\n",
 "ruby '2.5.1'\n",
 "gem 'rspec'\n",
 "gem 'pry'\n",
+"gem 'dotenv'\n",
 "gem 'rubocop', '~> 0.57.2'\n"]
-
-gem.each {|i| file << i }
-
+​
+gem.each {|i| file_gemfile << i }
+​
+#CREATION DES FICHIERS CACHES
+File.open(input + "/.env", "w")
+​
+file_gitignore = File.open(input + "/.gitignore", "w")
+file_gitignore << ".env"
+​
+#CREATION DU README
+file_readme = File.open(input + "/README.md", "w")
+file_readme << "# " + input.upcase
+​
+#INSTALLATION DES PROGRAMMES
+Dir.chdir(input)
+system("bundle install")
 system("git init")
-
-file = File.open(input + "/.env", "w")
-file = File.open(input + "/.gitignore", "w")
-file << ".env"
-
-Dir.mkdir(input + "/lib")
-
 system("rspec --init")
-
-file = File.open(input + "/README.md", "w")
-
 
 
 
